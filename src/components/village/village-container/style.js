49271c92d20fa732b755bd4ceb0efb47305f4fac
jin park <jin.park@rokolabs.com>
2020-07-13 15:45:09 -0300

    minor fix

--- src/components/village/village-container/style.js
+++ src/components/village/village-container/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const ELContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
-    margin-left: 20px;
+    margin-left: 70px;
     @media (max-width: ${props => props.theme.screen.sm}) {
         margin-left: 0px;
     }
